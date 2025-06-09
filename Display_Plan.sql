e223791447d0d6eb73b74874d3f555b923ce32a9
rijesh-chandran <rijesh.mc@outlook.com>
2025-06-09 22:52:18 +0530

    Display_Plan.sql

--- Display_Plan.sql
+++ Display_Plan.sql
@@ -1,2 +1,5 @@
 SET pages 999 lines 999
 SELECT * FROM   TABLE(DBMS_XPLAN.DISPLAY_CURSOR(sql_id=>'&sql_id',cursor_child_no=> &child_number, format => 'ADVANCED'));
+SELECT * FROM   TABLE(DBMS_XPLAN.DISPLAY_AWR(sql_id=>'&sql_id', plan_hash_value=> &plan_hash_value, db_id=>NULL, format => 'ADVANCED'));   
+SELECT * FROM   TABLE(DBMS_XPLAN.DISPLAY_SQL_PLAN_BASELINE(sql_handle=>'&sql_handle', plan_name=> '&plan_name', format => 'ADVANCED'));   
+
